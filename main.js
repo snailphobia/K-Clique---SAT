6b5a13022637f282242ea64f04ca348e1f69d4ba
snailphobia <cr.zlatea@gmail.com>
2023-01-03 23:31:30 +0200

    breed hatred and reign over it all

--- main.js
+++ main.js
@@ -24,7 +24,6 @@ function read() {
 				lt = line.split(' ')
 						.map(n => parseInt(n))
 						.filter(n => !isNaN(n));
-				console.log(lt);
 				V.push(lt);
 				if (i >= nV - 1) {
 					rl.removeListener('line', liner);
@@ -38,7 +37,52 @@ function read() {
 	});
 }
 
-read().then(() => {	
-	// can't wait to jsfuck this
-	
+read().then(() => {
+	V.push([]); // for the last vertex
+	for (let i = nV - 2; i >= 0; i--) {
+		V[i].forEach((item) => {
+			V[item - 1].push(i + 1); // add some duplicates to have them both ways
+			return;
+		});
+	}
+	V.forEach((item) => { item.sort(); });
+
+	let total_clauses = 0;
+	V.forEach((item) => {
+		let aux = nV - 1 - item.length;
+		if (aux < 0) aux = 0; // betrayal by your most prized friend
+		total_clauses += K * (K - 1) / 2 * aux;
+	});
+	total_clauses /= 2; total_clauses += nV + K; // it just works!
+
+	rl.output.write(`p cnf ${nV * K} ${total_clauses}\n`);
+
+	const idx = (i, j) => { return (i - 1) * nV + j; };
+
+	for (let i = 1; i <= K; i++) {
+		for (let j = 1; j <= nV; j++) {
+			rl.output.write(`${idx(i, j)} `);
+		}
+		rl.output.write('0\n');
+	}
+
+	for (let j = 1; j <= nV; j++) {
+		for (let i = 1; i <= K; i++) {
+			rl.output.write(`${idx(i, j)} `);
+		}
+		rl.output.write('0\n');
+	}
+
+	for (let v = 1; v <= nV; v++) {
+		for (let u = v + 1; u <= nV; u++) {
+			if (!V[v - 1].includes(u)) {
+				for (let i = 1; i <= K; i++) {
+					for (let j = i + 1; j <= K; j++) {
+							rl.output.write(`-${idx(i, v)} -${idx(j, u)} 0\n`);
+					}
+				}
+			}
+		}
+	}
+	// console.log(V);
 });
